344bc49e961eeae3d72af621aa25087ad701216f
Mikhail Khodjaiants <mikhailkhod@googlemail.com>
2014-01-29 16:59:11 -0500

    Implementation of the "Advanced Settings" element.

--- dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/newlaunch/DebuggerSettingsUIElement.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb.ui/src/org/eclipse/cdt/dsf/gdb/internal/ui/newlaunch/DebuggerSettingsUIElement.java
@@ -11,15 +11,36 @@
 
 package org.eclipse.cdt.dsf.gdb.internal.ui.newlaunch;
 
+import org.eclipse.cdt.debug.ui.dialogs.GridUtils;
 import org.eclipse.cdt.debug.ui.launch.AbstractUIElement;
+import org.eclipse.cdt.dsf.gdb.internal.ui.GdbUIPlugin;
+import org.eclipse.cdt.dsf.gdb.internal.ui.launching.LaunchUIMessages;
 import org.eclipse.cdt.dsf.gdb.newlaunch.DebuggerSettingsElement;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.PlatformUI;
 
 public class DebuggerSettingsUIElement extends AbstractUIElement {
 
+	private static String[] fgTraceModeLabels = new String[] {
+		LaunchUIMessages.getString("GDBDebuggerPage.tracepoint_mode_fast"), //$NON-NLS-1$
+		LaunchUIMessages.getString("GDBDebuggerPage.tracepoint_mode_normal"), //$NON-NLS-1$
+		LaunchUIMessages.getString("GDBDebuggerPage.tracepoint_mode_auto"), //$NON-NLS-1$
+	};
+
+	// Detail widgets
+	private Button fReverseButton;
+	private Button fUpdateThreadsButton;
+	private Button fDebugOnFork;
+	private Combo fTracepointModeCombo;
+	
 	public DebuggerSettingsUIElement(DebuggerSettingsElement launchElement, boolean showDetails) {
 		super(launchElement, showDetails);
 	}
@@ -37,14 +58,64 @@ public class DebuggerSettingsUIElement extends AbstractUIElement {
 	@Override
 	protected void doCreateDetailsContent(final Composite parent) {
 		Composite comp = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout(3, false);
+		GridLayout layout = new GridLayout(2, false);
 		layout.marginHeight = layout.marginWidth = 0;
 		comp.setLayout(layout);
 		comp.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));		
+		GridUtils.fillIntoGrid(comp, parent);
+		
+		fReverseButton = new Button(comp, SWT.CHECK);
+		GridUtils.fillIntoGrid(fReverseButton, comp);
+		fReverseButton.setText(LaunchUIMessages.getString("GDBDebuggerPage.reverse_Debugging")); //$NON-NLS-1$
+		fReverseButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				getLaunchElement().enableReverse(fReverseButton.getSelection());
+			}
+		});
+		
+		fUpdateThreadsButton = new Button(comp, SWT.CHECK);
+		GridUtils.fillIntoGrid(fUpdateThreadsButton, comp);
+		fUpdateThreadsButton.setText(LaunchUIMessages.getString("GDBDebuggerPage.update_thread_list_on_suspend")); //$NON-NLS-1$
+		fUpdateThreadsButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				getLaunchElement().setUpdateThreadListOnSuspend(fUpdateThreadsButton.getSelection());;
+			}
+		});
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(fUpdateThreadsButton, GdbUIPlugin.PLUGIN_ID + ".update_threadlist_button_context"); //$NON-NLS-1$
+		
+		fDebugOnFork = new Button(comp, SWT.CHECK);
+		GridUtils.fillIntoGrid(fDebugOnFork, comp);
+		fDebugOnFork.setText(LaunchUIMessages.getString("GDBDebuggerPage.Automatically_debug_forked_processes")); //$NON-NLS-1$
+		fDebugOnFork.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				getLaunchElement().setDebugOnFork(fDebugOnFork.getSelection());;
+			}
+		});
+		
+		Label label = new Label(comp, SWT.NONE);
+		label.setText(LaunchUIMessages.getString("GDBDebuggerPage.tracepoint_mode_label")); //$NON-NLS-1$
+		
+		fTracepointModeCombo = new Combo(comp, SWT.READ_ONLY | SWT.DROP_DOWN);
+		fTracepointModeCombo.setItems(fgTraceModeLabels);
 	}
 
 	@Override
 	protected void initializeDetailsContent() {
+		if (fReverseButton != null) {
+			fReverseButton.setSelection(getLaunchElement().isReverseEnabled());
+		}
+		if (fUpdateThreadsButton != null) {
+			fUpdateThreadsButton.setSelection(getLaunchElement().updateThreadListOnSuspend());
+		}
+		if (fDebugOnFork != null) {
+			fDebugOnFork.setSelection(getLaunchElement().isDebugOnFork());
+		}
+		if (fTracepointModeCombo != null) {
+			fTracepointModeCombo.select(getLaunchElement().getTracepointMode().ordinal());
+		}
 	}
 
 	@Override
--- dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/newlaunch/DebuggerSettingsElement.java
+++ dsf-gdb/org.eclipse.cdt.dsf.gdb/src/org/eclipse/cdt/dsf/gdb/newlaunch/DebuggerSettingsElement.java
@@ -15,13 +15,31 @@ import java.util.Map;
 
 import org.eclipse.cdt.debug.core.launch.AbstractLaunchElement;
 import org.eclipse.cdt.debug.core.launch.ILaunchElement;
+import org.eclipse.cdt.dsf.gdb.IGDBLaunchConfigurationConstants;
+import org.eclipse.cdt.dsf.gdb.newlaunch.OverviewElement.SessionTypeChangeEvent;
+import org.eclipse.cdt.dsf.gdb.service.SessionType;
 
 /**
  * @since 4.3
  */
 public class DebuggerSettingsElement extends AbstractLaunchElement {
 
-	final private static String ELEMENT_ID = ".debuggerSettings"; //$NON-NLS-1$
+	public enum TracepointMode {
+		FAST_ONLY,
+		NORMAL_ONLY,
+		AUTOMATIC,
+	}
+
+	final private static String ELEMENT_ID = ".advancedSettings"; //$NON-NLS-1$
+	final private static String ATTR_REVERSE = ".reverse"; //$NON-NLS-1$
+	final private static String ATTR_UPDATE_THREADLIST_ON_SUSPEND = ".updateThreadListOnSuspend"; //$NON-NLS-1$
+	final private static String ATTR_DEBUG_ON_FORK = ".debugOnFork"; //$NON-NLS-1$
+	final private static String ATTR_TRACEPOINT_MODE = ".tracepointMode"; //$NON-NLS-1$
+
+	private boolean fReverse = getDefaultReverseValue();
+	private boolean fUpdateThreads = getDefaultUpdateValue();
+	private boolean fDebugOnFork = getDefaultDebugOnForkValue();
+	private TracepointMode fTracepointMode = getDefaultTracepointMode();
 
 	public DebuggerSettingsElement(ILaunchElement parent) {
 		super(parent, parent.getId() + ELEMENT_ID, "Advanced Settings", "Debugger advanced settings");
@@ -35,18 +53,118 @@ public class DebuggerSettingsElement extends AbstractLaunchElement {
 
 	@Override
 	protected void doInitializeFrom(Map<String, Object> attributes) {
+		fReverse = getAttribute(attributes, getId() + ATTR_REVERSE, getDefaultReverseValue());
+		fUpdateThreads = getAttribute(attributes, getId() + ATTR_UPDATE_THREADLIST_ON_SUSPEND, getDefaultUpdateValue());
+		fDebugOnFork = getAttribute(attributes, getId() + ATTR_DEBUG_ON_FORK, getDefaultDebugOnForkValue());
+		fTracepointMode = TracepointMode.values()[getAttribute(attributes, getId() + ATTR_TRACEPOINT_MODE, getDefaultTracepointMode().ordinal())];
 	}
 
 	@Override
 	protected void doPerformApply(Map<String, Object> attributes) {
+		attributes.put(getId() + ATTR_REVERSE, fReverse);
+		attributes.put(getId() + ATTR_UPDATE_THREADLIST_ON_SUSPEND, fUpdateThreads);
+		attributes.put(getId() + ATTR_DEBUG_ON_FORK, fDebugOnFork);
+		attributes.put(getId() + ATTR_TRACEPOINT_MODE, fTracepointMode.ordinal());
 	}
 
 	@Override
 	protected void doSetDefaults(Map<String, Object> attributes) {
+		fReverse = getDefaultReverseValue();
+		fUpdateThreads = getDefaultUpdateValue();
+		fDebugOnFork = getDefaultDebugOnForkValue();
+		fTracepointMode = getDefaultTracepointMode();
+		attributes.put(getId() + ATTR_REVERSE, fReverse);
+		attributes.put(getId() + ATTR_UPDATE_THREADLIST_ON_SUSPEND, fUpdateThreads);
+		attributes.put(getId() + ATTR_DEBUG_ON_FORK, fDebugOnFork);
+		attributes.put(getId() + ATTR_TRACEPOINT_MODE, fTracepointMode.ordinal());
 	}
 
 	@Override
 	protected boolean isContentValid() {
 		return true;
+	}
+	
+	public static boolean getDefaultReverseValue() {
+		return IGDBLaunchConfigurationConstants.DEBUGGER_REVERSE_DEFAULT;
+	}
+	
+	public static boolean getDefaultUpdateValue() {
+		return IGDBLaunchConfigurationConstants.DEBUGGER_UPDATE_THREADLIST_ON_SUSPEND_DEFAULT;
+	}
+	
+	public static boolean getDefaultDebugOnForkValue() {
+		return IGDBLaunchConfigurationConstants.DEBUGGER_DEBUG_ON_FORK_DEFAULT;
+	}
+	
+	public static TracepointMode getDefaultTracepointMode() {
+		if (IGDBLaunchConfigurationConstants.DEBUGGER_TRACEPOINT_MODE_DEFAULT.equals(
+				IGDBLaunchConfigurationConstants.DEBUGGER_TRACEPOINT_FAST_ONLY)) {
+			return TracepointMode.FAST_ONLY;
+		}
+		if (IGDBLaunchConfigurationConstants.DEBUGGER_TRACEPOINT_MODE_DEFAULT.equals(
+				IGDBLaunchConfigurationConstants.DEBUGGER_TRACEPOINT_NORMAL_ONLY)) {
+			return TracepointMode.NORMAL_ONLY;
+		}
+		return TracepointMode.AUTOMATIC;
+	}
+
+	public boolean isReverseEnabled() {
+		return fReverse;
+	}
+
+	public void enableReverse(boolean reverse) {
+		if (reverse == fReverse) {
+			return;
+		}
+		fReverse = reverse;
+		elementChanged(CHANGE_DETAIL_STATE);
+	}
+
+	public boolean updateThreadListOnSuspend() {
+		return fUpdateThreads;
+	}
+
+	public void setUpdateThreadListOnSuspend(boolean update) {
+		if (update == fUpdateThreads) {
+			return;
+		}
+		fUpdateThreads = update;
+		elementChanged(CHANGE_DETAIL_STATE);
+	}
+
+	public boolean isDebugOnFork() {
+		return fDebugOnFork;
+	}
+
+	public void setDebugOnFork(boolean debugOnFork) {
+		if (fDebugOnFork == debugOnFork) {
+			return;
+		}
+		fDebugOnFork = debugOnFork;
+		elementChanged(CHANGE_DETAIL_STATE);
+	}
+
+	public TracepointMode getTracepointMode() {
+		return fTracepointMode;
+	}
+
+	public void setTracepointMode(TracepointMode tracepointMode) {
+		if (fTracepointMode == tracepointMode ) {
+			return;
+		}
+		fTracepointMode = tracepointMode;
+		elementChanged(CHANGE_DETAIL_STATE);
+	}
+
+	@Override
+	public void update(IChangeEvent event) {
+		if (event instanceof SessionTypeChangeEvent) {
+			handleSessionTypeChange((SessionTypeChangeEvent)event);
+		}
+		super.update(event);
+	}
+
+	private void handleSessionTypeChange(SessionTypeChangeEvent event) {
+		 setEnabled(!SessionType.CORE.equals(event.getNewType()));
 	}
 }
